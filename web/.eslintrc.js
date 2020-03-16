2c91d13c71e998e09c01225855284181d0dd20f0
Wladimir <wladimir.code@gmail.com>
2020-03-15 21:42:45 -0300

    feat: Styles config, layout, spreading props off

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
       { argsIgnorePattern: "^_" }
     ],
     "react/jsx-one-expression-per-line": "off",
+    "react/jsx-props-no-spreading": "off",
     "global-require": "off",
     "react-native/no-raw-text": "off",
     "no-param-reassign": "off",
