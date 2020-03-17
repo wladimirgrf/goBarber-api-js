c49f83ecaf61338c1b20819545fb05f9189a7ed6
Wladimir <wladimir.code@gmail.com>
2020-03-17 19:56:38 -0300

    feat: Redux session controller, axios config, auth state

--- web/src/routes/Route.js
+++ web/src/routes/Route.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import store from '~/store';
 import AuthLayout from '~/pages/layout/auth';
 import DefaultLayout from '~/pages/layout/default';
 
@@ -10,7 +11,7 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
