5609afb3bf93753ad2a383d93b907fdbb51307bc
Wladimir <wladimir.code@gmail.com>
2020-03-16 22:10:58 -0300

    feat: SigIn & SingUp styles, logo, polished

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Login</button>
+        <Link to="/register">Sign up today</Link>
+      </form>
+    </>
+  );
 }
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Full Name" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Sign up</button>
+        <Link to="/">Log in</Link>
+      </form>
+    </>
+  );
 }
