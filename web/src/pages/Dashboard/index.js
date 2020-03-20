6c288c479e2453fc2fa7a190531393c835b7d95c
Wladimir <wladimir.code@gmail.com>
2020-03-20 20:21:06 -0300

    feat: Dashboard components, styles

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Valdir klarken</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Available</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Valdir klarken</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Valdir klarken</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
